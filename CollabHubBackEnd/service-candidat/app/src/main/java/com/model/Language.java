ce63afada966a9e72bb5b3292a9c209661a0eb6f
Molid <delmolid@hotmail.fr>
2025-06-13 17:40:56 +0200

    feature/création du model,repository,service et controlleur pour le métier candidat et creation premier donnée api

---
+++ CollabHubBackEnd/service-candidat/app/src/main/java/com/model/Language.java
@@ -0,0 +1,14 @@
+package com.model;
+
+public enum Language {
+    FRANCAIS,
+    ANGLAIS,
+    ESPAGNOL,
+    ALLEMAND,
+    ITALIEN,
+    PORTUGAIS,
+    ARABE,
+    CHINOIS,
+    JAPONAIS,
+    RUSSE
+}
